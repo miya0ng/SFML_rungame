dc0d561f3ac907206041d8809d0f56f4a59c3ba0
이승연 <leelaf0002@gmail.com>
2025-07-15 14:36:48 +0900

    reverted linear tile pattern

--- StageManager.cpp
+++ StageManager.cpp
@@ -1,45 +1,6 @@
 ﻿#include "stdafx.h"
 #include "StageManager.h"
 
-inline StageManager::TilePattern MakeFlat(TileType type, int n)
-{
-	return [n, type](StageManager& mgr)
-		{
-			for (int i = 0; i < n; ++i)
-				mgr.SpawnTile(type);
-		};
-}
-
-inline StageManager::TilePattern MakeGap(TileType type, int g)
-{
-	return [g, type](StageManager& mgr)
-		{
-			mgr.lastX += mgr.tileW * g;
-		};
-}
-
-inline StageManager::TilePattern MakeStair(TileType type, int n, float step)
-{
-	return [type, n, step](StageManager& mgr)
-		{
-			float y = mgr.baseY;
-			for (int i = 0; i < n; ++i)
-			{
-				auto t = mgr.SpawnTile(type);
-				t->SetPosition({ mgr.lastX, y });
-				mgr.lastX += mgr.tileW;
-				y -= step;
-			}
-		};
-}
-
-std::vector<TimedPattern> patternTimeline =
-{
-	{ 0.f,   3.f,  MakeFlat(TileType::Ground, 10) },
-	{ 3.f,  5.f,  MakeGap(TileType::Ground, 3) },
-	{5.f,  10.f,  MakeStair(TileType::Ground, 3, 40.f) },
-};
-
 StageManager::StageManager()
 {
 }
@@ -69,46 +30,27 @@ Platform* StageManager::SpawnTile(TileType type)
 	newTile->Init();
 	newTile->SetType(type);
 
-	startX = activeTiles.empty() ? 0.f : activeTiles.back()->GetPosition().x;
+	float startX = activeTiles.empty() ? 0.f : activeTiles.back()->GetPosition().x + tileSprite.getGlobalBounds().width;
 
 	newTile->SetPosition({ startX, 300 });
 	newTile->SetScale({ 0.7f, 0.7f });
 	activeTiles.push_back(newTile);
 
-	lastX = startX + tileW;
 	return newTile;
 }
 
 void StageManager::Update(float dt, float playerSpeed)
 {
-	for (const auto& tp : patternTimeline)
-	{
-	}
+	float tileWidth = tileSprite.getGlobalBounds().width;
+	float tileSpawnTriggerX = FRAMEWORK.GetWindowBounds().width - tileWidth;
 
-	MoveTiles(dt, playerSpeed);
-}
-
-void StageManager::Draw(sf::RenderWindow& win)
-{
-	for (auto* t : activeTiles)
-		t->Draw(win);
-}
-
-Platform* StageManager::SpawnPattern(TileType type)
-{
-
-	return SpawnTile(type);
-}
-
-void StageManager::MoveTiles(float dt, float playerSpeed)
-{
 	for (auto it = activeTiles.begin(); it != activeTiles.end(); )
 	{
 		auto tile = *it;
 		pos = tile->GetPosition();
 		pos.x += dt * playerSpeed * dir;
-		tile->SetPosition({ pos.x, baseY });
-		if (pos.x < -tileW)//-----------------------------------------tile->SetActive(false)
+		tile->SetPosition({ pos.x, newTile->GetPosition().y });
+		if (pos.x < -tileWidth)//------------------------------------tile->SetActive(false)
 		{
 			tile->SetActive(false);
 			pooledTiles.push_back(tile);
@@ -119,51 +61,15 @@ void StageManager::MoveTiles(float dt, float playerSpeed)
 			++it;
 		}
 	}
-	if (NeedNewTiles())
+
+	if (pos.x < tileSpawnTriggerX)
 	{
-		for (const auto& tp : patternTimeline)
-		{
-			if ()
-			{
-				break;
-			}
-		}
+		SpawnTile(TileType::Ground);
 	}
 }
 
-bool StageManager::NeedNewTiles() const
+void StageManager::Draw(sf::RenderWindow& win)
 {
-	return lastX < FRAMEWORK.GetWindowBounds().width;
+	for (auto* t : activeTiles)
+		t->Draw(win);
 }
-
-//void StageManager::SpawnPattern(TileType type)
-//{
-//	switch (type)
-//	{
-//	case TileType::Ground:       SpawnFlat();      break;
-//	case TileType::Gap:			 SpawnGap();       break;
-//	case TileType::Floating:     SpawnFloating();  break;
-//	}
-//}
-//
-//void StageManager::SpawnFlat()
-//{
-//	for (int i = 0; i < 5; ++i)
-//	SpawnTile(TileType::Ground);
-//}
-//void StageManager::SpawnGap()
-//{
-//	SpawnTile(TileType::Ground);
-//	lastX += tileWidth * 2;
-//}
-//
-//void StageManager::SpawnStair()
-//{
-//	float y = 300;
-//	for (int step = 0; step < 4; ++step)
-//	{
-//		SpawnTile(TileType::Ground)->SetPosition({ lastX, y });
-//		lastX += tileWidth;
-//		y = 100; // 한 칸씩 위로
-//	}
-//}
